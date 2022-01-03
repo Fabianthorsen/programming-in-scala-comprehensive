eca1508317654dc24dcbdb39941152f1e95df281
Fabian Thorsen <thorsen.fabian@gmail.com>
2022-01-03 14:30:40 +0100

    New files

---
+++ Code/src/main/scala/FunctionsAndClosures/Methods/LongLines.scala
@@ -0,0 +1,16 @@
+package FunctionsAndClosures.Methods
+import scala.io.Source
+
+object LongLines {
+  def processFile(filename: String, width: Int): Unit =
+    // Local function instead of private def ...
+    // This helps to not pollute namespace
+    def processLine(line: String): Unit = {
+      if (line.length > width)
+        println(s"$filename: ${line.trim}")
+    }
+
+    val source = Source.fromFile(filename)
+    for (line <- source.getLines())
+      processLine(filename, width, line)
+}
